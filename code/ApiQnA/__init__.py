b9c274ba83b9f78027379d6de40f65c567302a31
Ernesto Botella Vélez <ernesto.botella@gmail.com>
2023-07-04 15:19:42 +0200

    try to solve error: Result: Failure Exception: AttributeError: 'str' object has no attribute 'get'

--- code/ApiQnA/__init__.py
+++ code/ApiQnA/__init__.py
@@ -1,5 +1,7 @@
 import azure.functions
 from dotenv import load_dotenv
+import logging
+import json
 load_dotenv()
 
 import os
@@ -10,22 +12,25 @@ def main(req: azure.functions.HttpRequest) -> str:
     # Get data from POST request
     try:
         req_body = req.get_json()
+        logging.info ("ApiQnA req_body: " + req_body)
+        logging.info ("ApiQnA type(req_body): " + type(req_body))
+        req_body = json.loads(req_body)
     except ValueError as error:
-        print("ApiQnA an exception occurred:", error) 
+        logging.info("ApiQnA an exception occurred:", error) 
         pass
     else:
-        print ("ApiQnA 2")
+        logging.info ("ApiQnA 2")
         question = req_body.get('question')
         history = req_body.get('history', [])
         custom_prompt = req_body.get('custom_prompt', "")
         custom_temperature = float(req_body.get('custom_temperature', os.getenv("OPENAI_TEMPERATURE", 0.7)))
-    print ("ApiQnA 3")
+    logging.info ("ApiQnA 3")
     # Create LLMHelper object
     llm_helper = LLMHelper(custom_prompt=custom_prompt, temperature=custom_temperature)
     # Get answer
-    print ("ApiQnA 4")
+    logging.info ("ApiQnA 4")
     data = {}
     data['question'], data['response'], data['context'], data["sources"] = llm_helper.get_semantic_answer_lang_chain(question, history)
     # Return answer
-    print ("ApiQnA 5 Fin")
+    logging.info ("ApiQnA 5 Fin")
     return f'{data}'
